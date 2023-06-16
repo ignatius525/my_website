0661e333fd79e03d236be9ff2653316e73148cc1
Ignat Miagkov <iamiagkov@email.wm.edu>
2023-06-15 23:46:18 -0400

    Add low poly spaceship

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,15 +5,41 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
 
 const Computers = () => {
-  const computer = useGLTF('./desktop_pc/scene.gltf')
+  const computer = useGLTF('./pixel_low_poly_spaceship/scene.gltf')
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15}
+      <hemisphereLight intensity={0.05}
       groundColor="black" />
-      <pointLight intensity={1}/>
+      <pointLight intensity={1} />
+      <primitive object={computer.scene} 
+       scale={1}
+       position={[5,2.25, 1.5]}
+       rotation={[-0.01, -0.2, -0.1]} 
+      />
     </mesh>
   )
 }
 
-export default Computers
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25}}
+      gl={{ preserveDrawingBuffer: true }}>
+        <Suspense fallback={<CanvasLoader />}>
+          <OrbitControls 
+           enableZoom={false} 
+           maxPolarAngle={Math.PI / 2}
+           minPolarAngle={Math.PI / 2}
+          />
+          <Computers />
+        </Suspense>
+
+        <Preload all />
+    </Canvas>
+  )
+}
+
+export default ComputersCanvas;
