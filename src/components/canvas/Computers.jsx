e663e1a2012c72b64299e7974e991085b0cbc853
Ignat Miagkov <ignatiusa.525@gmail.com>
2023-06-17 14:41:33 -0400

    try glb for spaceship

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,7 +5,7 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
 
 const Computers = () => {
-  const computer = useGLTF('desktop_pc/scene.gltf')
+  const computer = useGLTF('pixel_low_poly_spaceship.glb')
 
   return (
     <mesh>
@@ -14,8 +14,8 @@ const Computers = () => {
       <pointLight intensity={1} />
       <primitive object={computer.scene} 
        scale={0.75}
-       position={[0,-3.25, -1.5]}
-       rotation={[-0.01, -0.2, -0.1]} 
+       position={[5, 2, -1.5]}
+       rotation={[.3, -0.4, -0.2]} 
       />
     </mesh>
   )
@@ -26,7 +26,7 @@ const ComputersCanvas = () => {
     <Canvas
       frameloop="demand"
       shadows
-      camera={{ position: [20, 3, 5], fov: 25}}
+      camera={{ position: [20, 3, 5], fov: 10}}
       gl={{ preserveDrawingBuffer: true }}>
         <Suspense fallback={<CanvasLoader />}>
           <OrbitControls 
