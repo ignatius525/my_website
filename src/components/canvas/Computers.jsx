f67015d01521da7a5f94ebb2fd495675060c5399
Ignat Miagkov <ignatiusa.525@gmail.com>
2023-06-19 01:00:08 -0400

    marcus aurelius

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,7 +5,7 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF('desktop_pc/scene.gltf')
+  const computer = useGLTF('marcus_aurelius/scene.gltf')
 
   return (
     <mesh>
@@ -21,8 +21,8 @@ const Computers = ({ isMobile }) => {
         shadow-mapSize={1024}
       />
       <primitive object={computer.scene} 
-       scale={isMobile ? 0.6 : 0.75}
-       position={isMobile ? [0, -2.5, -1.7] : [0, -3.25, -1.5]}
+       scale={isMobile ? 0.6 : 5}
+       position={isMobile ? [0, -2.5, -1.7] : [0, 0, 0]}
        rotation={[-0.01, -0.2, -0.1]} 
       />
     </mesh>
