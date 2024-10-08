aecc0dbede5422f4558bbcd39ea66b8e1139c5c0
Ignat Miagkov <ignatiusa.525@gmail.com>
2024-10-07 22:20:11 -0400

    try again

--- src/components/OpenGraph.jsx
+++ src/components/OpenGraph.jsx
@@ -4,11 +4,11 @@ const OpenGraph = () => {
     return ( 
       <div> 
         <Helmet> 
-          <meta property="og:title" content="Thailand’s celebrity pygmy hippo Moo-Deng gets shot by vistor, in critical condition" /> 
+          <meta property="og:title" content="Thai celebrity pygmy hippo Moo-Deng gets shot by vistor, in critical condition" /> 
           {/* <meta property="og:description" content="Description of your content." />  */}
           <meta property="og:image" content="https://www.hindustantimes.com/ht-img/img/2024/09/16/550x309/Moo_Deng_1726453915615_1726453915979.jpg"/> 
-          <meta property="og:url" content="https://nypost.com/" /> 
-          <meta property="og:type" content="website" /> 
+          {/* <meta property="og:url" content="https://nypost.com/" /> 
+          <meta property="og:type" content="website" />  */}
         </Helmet> 
       </div> 
     ); 
